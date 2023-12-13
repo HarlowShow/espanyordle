2e6043f39f1d748980d10262ba73babaef6b32a9
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2023-12-13 17:39:52 +0000

    base stats component setup

--- app/game/results/stats.jsx
+++ app/game/results/stats.jsx
@@ -1,12 +1,33 @@
 import styles from "./styles.module.css";
+import { getStatsFromLocalStorage } from "../../data/localstorage.js";
 
-const Stats = (() => {
-
-
-    return (
-        <div className={styles['content-wrapper']}>
-            <h2 className={styles['heading']}>Statistics</h2>
+const Stats = () => {
+  const stats = getStatsFromLocalStorage();
+  const winPerc = stats.won / stats.played * 100
+  return (
+    <div className={styles["content-wrapper"]}>
+      {stats ? (
+        <div>
+          <h2 className={styles["heading"]}>Statistics</h2>
+          <ul className={styles['key-stats-wrapper']}>
+            <li className={styles['key-stat']}>
+                <p>{ stats.played }</p>
+                <p>Played</p>
+            </li>
+            <li className={styles['key-stat']}>
+                <p>{ winPerc }%</p>
+                <p>Games Won</p>
+            </li>
+            
+          </ul>
         </div>
-    )
-})
-export default Stats
+      ) : (
+        <div>
+          <h2 className={styles["heading"]}>No Statistics Found</h2>
+          <p>Play some games to see your statistics</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Stats;
