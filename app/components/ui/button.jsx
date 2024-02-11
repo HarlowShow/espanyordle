0f600a2c257d023a14f3d4e48d3ee1f4781124a5
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2024-02-11 11:53:47 +0000

    cleanup unused components

--- app/components/ui/button.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Buttons.module.css'
-
-export default function Button({callback, label, children}) {
-    return (
-        <button type="button" aria-label={label} className={styles['button']} onClick={callback}>{children}</button>
-    )
-}
