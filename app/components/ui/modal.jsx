2b3e1bd17a6dbf2fd8c9e715a7d6f4a24200a992
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2024-02-17 22:09:40 +0000

    add scroll-lock to modal

--- app/components/ui/modal.jsx
+++ app/components/ui/modal.jsx
@@ -1,41 +1,44 @@
-'use client'
+"use client";
 
-import styles from './Modal.module.css'
+import styles from "./Modal.module.css";
 import { IoClose } from "react-icons/io5";
-import IconButton from './iconbutton';
-import FocusLock from 'react-focus-lock';
-import { useEffect } from 'react'
+import IconButton from "./iconbutton";
+import FocusLock from "react-focus-lock";
+import { RemoveScroll } from "react-remove-scroll";
+import { useEffect } from "react";
 
-export default function Modal({ handleClose, title, children}) {
+export default function Modal({ handleClose, title, children }) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === "Escape") {
+        handleClose();
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleClose]);
 
-
-    useEffect(() => {
-
-        function handleKeyDown(event) {
-          if (event.code === 'Escape') {
-            handleClose();
-          }
-        }
-        window.addEventListener('keydown', handleKeyDown);
-        return () => {
-          window.removeEventListener('keydown', handleKeyDown);
-        };
-      }, [handleClose]);
-
-    return (
-        <FocusLock returnFocus={true}>
-        <div className={styles['modal-wrapper']}>
-            <div className={styles['modal']} role="dialog" aria-modal="true" aria-label={title}>
-            <div className={styles['modal-header']}>
-                <IconButton callback={handleClose} label={'close'}>
-                   <IoClose aria-hidden="true" focusable="false"/>
-                </IconButton>
+  return (
+    <FocusLock returnFocus={true}>
+      <RemoveScroll>
+        <div className={styles["modal-wrapper"]}>
+          <div
+            className={styles["modal"]}
+            role="dialog"
+            aria-modal="true"
+            aria-label={title}
+          >
+            <div className={styles["modal-header"]}>
+              <IconButton callback={handleClose} label={"close"}>
+                <IoClose aria-hidden="true" focusable="false" />
+              </IconButton>
             </div>
-            <div className={styles['modal-content']}>
-                    { children }
-            </div>
-            </div>
+            <div className={styles["modal-content"]}>{children}</div>
+          </div>
         </div>
-        </FocusLock>
-    )
+      </RemoveScroll>
+    </FocusLock>
+  );
 }
