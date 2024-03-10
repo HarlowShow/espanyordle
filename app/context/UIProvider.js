219ddaabdcd38db45837d2f8de39ded8355655b9
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2024-03-10 14:27:25 +0000

    turn off test modal setting

--- app/context/UIProvider.js
+++ app/context/UIProvider.js
@@ -7,7 +7,7 @@ export const UIContext = createContext();
 function UIProvider({children}) {
 
     const [showHelpModal, setShowHelpModal] = useState(false)
-    const [showResultsModal, setShowResultsModal] = useState(true)
+    const [showResultsModal, setShowResultsModal] = useState(false)
 
     return (
         <UIContext.Provider
