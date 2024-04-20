2f921e1dd3c1dbb3bec4500107098be2430ddf9a
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2024-04-20 13:56:16 +0100

    update zero_day date

--- app/data/config.js
+++ app/data/config.js
@@ -1,6 +1,6 @@
 // remember to set the date BACK to simulate time moving forward
 // also remember that the month is 0 indexed
-export const ZERO_DAY_WORDLE = new Date(2024, 1, 17)
+export const ZERO_DAY_WORDLE = new Date(2024, 3, 5)
 
 // indexes to search from in case of fallback words
 export const MAX_EASY_INDEX = 80
