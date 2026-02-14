5a33748ab5bbe0c01512000f9428ef7d0b774e13
HarlowShow <83132314+HarlowShow@users.noreply.github.com>
2026-02-14 12:46:54 +0000

    security updates

--- app/data/config.js
+++ app/data/config.js
@@ -1,6 +1,6 @@
 // remember to set the date BACK to simulate time moving forward
 // also remember that the month is 0 indexed
-export const ZERO_DAY_WORDLE = new Date(2024, 7, 1);
+export const ZERO_DAY_WORDLE = new Date(2026, 1, 1);
 
 // indexes to search from in case of fallback words
 export const MAX_EASY_INDEX = 80;
